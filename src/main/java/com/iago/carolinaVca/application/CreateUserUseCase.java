5d4734fec98e44c3f65924a2b9184c969b2bedcb
Iagoti <iagoti03@gmail.com>
2026-01-06 22:59:21 -0300

    fix: retornando DTO, response, request

--- src/main/java/com/iago/carolinaVca/application/CreateUserUseCase.java
+++ src/main/java/com/iago/carolinaVca/application/CreateUserUseCase.java
@@ -2,15 +2,27 @@ package com.iago.carolinaVca.application;
 
 import com.iago.carolinaVca.domain.User;
 import com.iago.carolinaVca.domain.repositories.IUserRepository;
+import com.iago.carolinaVca.presentation.mapper.UserPresentationMapper;
+import com.iago.carolinaVca.presentation.request.CreateUserRequest;
+import com.iago.carolinaVca.presentation.response.UserResponse;
 
 public class CreateUserUseCase {
-    private final IUserRepository usuarioRepository;
 
-    public CreateUserUseCase(IUserRepository usuarioRepository) {
+    private final IUserRepository usuarioRepository;
+    private final UserPresentationMapper userPresentationMapper;
+
+    public CreateUserUseCase(
+            IUserRepository usuarioRepository,
+            UserPresentationMapper userPresentationMapper
+    ) {
         this.usuarioRepository = usuarioRepository;
+        this.userPresentationMapper = userPresentationMapper;
     }
 
-    public User execute(User user) {
-        return usuarioRepository.save(user);
+    public UserResponse execute(CreateUserRequest request) {
+        User userRequest = userPresentationMapper.toDomain(request);
+        User newUser = usuarioRepository.save(userRequest);
+        UserResponse userResponse = userPresentationMapper.toResponse(newUser);
+        return userResponse;
     }
 }
